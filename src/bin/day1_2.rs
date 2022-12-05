f0a6f6991811e8ab9935dfb1c2064f78d295ea88
fnurgel <fnurgel@gmail.com>
2022-12-05 20:54:31 +0100

    rustified

--- src/bin/day1_2.rs
+++ src/bin/day1_2.rs
@@ -8,16 +8,15 @@ fn main() {
         .map(|s| s.trim().parse::<i128>())
         .collect::<Vec<_>>();
 
-    let mut result = cal.iter().fold(vec![0], |acc, element | {
-        let mut new_acc = acc.clone();
-        if element.is_ok() {
-            if acc.len() > 0 {
-                new_acc[acc.len() - 1] += element.as_ref().unwrap();
-            }
-        } else {
-            new_acc.push(0);
+    let mut result = cal.iter().fold(vec![0], |mut acc, element | {
+        match element {
+            Ok(v) => {
+                let last_pos = acc.len() - 1;
+                acc[last_pos] += v;
+            },
+            _err => acc.push(0)
         }
-        new_acc
+        acc
     });
     result.sort();
     let three_highest = &result[result.len() - 3..];
